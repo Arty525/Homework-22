c052ff5616da82bb31f3244d8581f679d36caac8
Andrey <89473462+Arty525@users.noreply.github.com>
2022-07-21 20:01:16 +0300

    Add files via upload

--- EX2.cpp
+++ EX2.cpp
@@ -6,22 +6,42 @@ int main() {
 	std::map<int, std::string> queue;
 	std::string surname;
 	std::string command;
+	std::string next;
+	int n = 0;
 	
-	for (int n = 0; command != "exit"; ++n) {
+	do {
 		std::cout << "Enter data: ";
 		std::cin >> command;
-		if (command != "next") {
+		if (command == "Exit" || command == "exit") {
+			break;
+		}
+		if ((command != "next" || command != "Next") && (command[0] >= 'A' && command[0] <= 'Z')) {
 			surname = command;
 			queue.insert(std::pair<int, std::string>(n, surname));
 			std::cout << "Data recorded." << std::endl;
+			++n;
 		}
-		else {
-			for (std::map<int, std::string>::iterator it = queue.begin(); it != queue.end(); ++it) {
-				if (it->second == surname) std::cout << it->second << std::endl;
+		else if (command == "next" || command == "Next") {
+			if (queue.size() > 0) {
+				int num = queue.begin()->first;
+				next = queue.begin()->second;
+				for (std::map<int, std::string>::iterator it = queue.begin(); it != queue.end(); ++it) {
+					if (it->second < next && it != queue.end()) {
+						next = it->second;
+						num = it->first;
+					}
+				}
+				std::cout << next << std::endl;
+				queue.erase(num);
+			}
+			else {
+				std::cout << "There is no one in the queue" << std::endl;
 			}
 		}
-	}
-
-	//Цикл сортировки аналогичный сортировке векторов
+		else if ((command != "Exit" || command != "exit") && (command[0] < 'A' || command[0] > 'Z')) {
+			std::cout << "Surname is capitalized." << std::endl;
+		}
+	} while (command != "exit" || command != "Exit");
+	
 	return 0;
 }
